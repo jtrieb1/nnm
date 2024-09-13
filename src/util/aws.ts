6f1fdcadca9c632eb39345edc6256bcc0e601a6f
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2024-09-13 19:24:24 -0400

    Removed unnecessary code since we will host on EC2

--- src/util/aws.ts
+++ src/util/aws.ts
@@ -2,30 +2,10 @@ import { CreateAccessKeyCommand, IAMClient } from "@aws-sdk/client-iam";
 import { S3Client } from "@aws-sdk/client-s3";
 
 async function getS3Client(): Promise<S3Client> {
-    const iamClient = new IAMClient({});
-
-    const createAccessKeyResponse = await iamClient.send(new CreateAccessKeyCommand({
-        UserName: "nnmSite",
-    }));
-
-    if (
-        !createAccessKeyResponse.AccessKey?.AccessKeyId ||
-        !createAccessKeyResponse.AccessKey?.SecretAccessKey
-    ) {
-        throw new Error("Access key not created");
-    }
-
-    const {
-        AccessKey: { AccessKeyId, SecretAccessKey },
-    } = createAccessKeyResponse;
-
-    let s3 =  new S3Client({
-        region: "us-east-1",
-        credentials: {
-            accessKeyId: AccessKeyId,
-            secretAccessKey: SecretAccessKey,
-        },
+    let s3 = new S3Client({
+        region: "us-east-1"
     });
+
     return s3; 
 }
 
