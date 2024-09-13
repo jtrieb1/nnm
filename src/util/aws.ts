3843d1bd8101b1187bebbd0d95751e3f74d28486
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2024-09-13 19:57:17 -0400

    Trying API key stuff

--- src/util/aws.ts
+++ src/util/aws.ts
@@ -1,10 +1,16 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
 
 async function getS3Client(): Promise<S3Client> {
 
-    let s3 = new S3Client({
-        region: "us-east-1"
-    });
+    let sc: S3ClientConfig = {
+        region: "us-east-1",
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? ""
+        }
+    };
+    
+    let s3 = new S3Client(sc);
 
     return s3;
 }
