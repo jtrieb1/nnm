d242719acce88b3053cbf0eb567e9523b200a8e2
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2024-10-11 11:54:02 -0400

    Added standard bg color to full page, added Layout component to index

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,18 @@
 import ContextBg from "../bg/ContextBg";
 import Footer from "./Footer";
 import Header from "./Header";
+import "./Layout.css";
 
 import React, { ReactNode } from 'react';
 
 const Layout: React.FC<{children: ReactNode[], clipbg: boolean}> = ({ children, clipbg }) => {
     return (
-        <>
+        <div className="layout">
             <Header />
             <ContextBg clip={clipbg} />
             {children}
             <Footer />
-        </>
+        </div>
     );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,17 @@ import type { HeadFC, PageProps } from "gatsby"
 
 import Hero from "../components/hero/Hero"
 import NewsContainer from "../components/news/NewsContainer"
-import Header from "../components/layout/Header"
-import Footer from "../components/layout/Footer"
+import Layout from "../components/layout/Layout"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <>
-      <Header />
+    <Layout clipbg>
       <Hero
         title="no nothing magazine"
         description="The magazine about nothing, but in a legally distinct way."
       />
       <NewsContainer />
-      <Footer />
-    </>
+    </Layout>
   )
 }
 
